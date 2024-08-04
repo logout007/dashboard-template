168f4922b674a79dfab0164bd24bea2430492b87
Pinaki Batabyal <pinakibatabyal@gmail.com>
2024-08-04 12:10:06 +0530

    css changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './Header.css';
+import { MdEmail } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
+import { FaRegBell } from "react-icons/fa";
+import { IoPerson } from "react-icons/io5";
+
 
 const Header = () => {
   return (
     <header className="header">
       <input type="text" placeholder="Search" />
       <div className="header-icons">
-        {/* Replace with actual icons */}
-        <span>🔔</span>
-        <span>💌</span>
-        <span>👤</span>
+        <span><MdEmail /></span>
+        <span><IoSettingsOutline /></span>
+        <span><FaRegBell /></span>
+        <span><IoPerson /></span>
+
       </div>
     </header>
   );
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './Sidebar.css';
+import { IoMdHome } from "react-icons/io";
+import { MdInsertChartOutlined } from "react-icons/md";
+import { TbReportAnalytics } from "react-icons/tb";
+import { CiWallet } from "react-icons/ci";
+import { IoBagCheckSharp } from "react-icons/io5";
+import { IoLogoReact } from "react-icons/io5";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="logo">
-        {/* Replace with logo icon */}
-        <span>🌐</span>
+      <IoLogoReact />
       </div>
       <div className="menu">
-        <div className="menu-item">🏠</div>
-        <div className="menu-item">📊</div>
-        <div className="menu-item">⚙️</div>
-        <div className="menu-item">🔔</div>
+        <div className="menu-item"><IoMdHome /></div>
+        <div className="menu-item"><MdInsertChartOutlined /></div>
+        <div className="menu-item"><TbReportAnalytics /></div>
+        <div className="menu-item"><CiWallet /></div>
+        <div className="menu-item"><IoBagCheckSharp /></div>
+
       </div>
     </div>
   );
